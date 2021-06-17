7b2fdcaa77815abc8e13a71af3c2572d21e2de10
moo9205 <angus920514@gmail.com>
2021-06-18 00:29:11 +0900

    not solved 5/7 passed

--- Toy/toy3.js
+++ Toy/toy3.js
@@ -1,3 +1,9 @@
 const isSubsetOf = function (base, sample) {
   // TODO: 여기에 코드를 작성합니다.
+  for (let i = 0; i < this.length; i++) {
+    if (!array.includes(this[i])) {
+      return false;
+    }
+  }
+  return true;
 };
