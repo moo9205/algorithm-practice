33a871a0a2bfa9b5cd3a438df3328ee2691173a4
moo9205 <angus920514@gmail.com>
2021-06-19 19:05:59 +0900

    solve

---
+++ Toy/toy4.js
@@ -0,0 +1,14 @@
+const bubbleSort = function (arr) {
+  let length = arr.length;
+  let temp;
+  for (let i = 0; i < length - 1; i++) {
+    for (let j = 0; j < length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+};
