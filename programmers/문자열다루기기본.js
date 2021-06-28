e4c1ee755afe43d5f216bc3c49978399ba74f92a
moo9205 <angus920514@gmail.com>
2021-06-29 00:12:50 +0900

    solved

--- programmers/문자열다루기기본.js
+++ programmers/문자열다루기기본.js
@@ -1,8 +1,12 @@
 function solution(s) {
-  if (s.length === 4 || s.length === 6) {
-    return !isNaN(s);
+  let arr = s.split("");
+  if (arr.length !== 4 && arr.length !== 6) {
+    return false;
   }
-  return false;
+  for (let i = 0; i < arr.length; i++) {
+    if (isNaN(Number(arr[i]))) {
+      return false;
+    }
+  }
+  return true;
 }
-
-console.log(solution("a234"));
